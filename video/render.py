538b77e8f740574b1c0d81e6b81b6ab2dd0ffdab
Amaury Carrade <amaury@carrade.eu>
2017-10-30 17:04:03 +0100

    Split render in multiple passes to avoid memory issues

--- video/render.py
+++ video/render.py
@@ -16,13 +16,6 @@ root, _ = os.path.split(os.path.abspath(__file__))
 root += '/'
 
 
-'''
-Renders a video with data on it.
-
-If the 'no-messages' flag is passed, flashing messages are not written on the video.
-'''
-
-
 PROGRESS_BAR_HEIGHT = 10
 PROGRESS_BAR_WIDTH = 1363
 PROGRESS_BAR_POSITION = (41, 1000)
@@ -37,25 +30,41 @@ TEMPERATURE_POSITION = (1845, 956)
 TEMPERATURE_POSITION_MISSING = (1845, 972)
 
 
-clip = mpy.VideoFileClip(root + '../../Enregistrements/Embarqué/CameraBallon-complet.mp4')
-clip_gui = mpy.ImageClip(root + '../data/video/cadre_données_vide.png').set_duration(clip.duration)
-
-composition = [clip, clip_gui]
-
-
-# ------  Data on the video
+# Loads data in second-indexed dict
+aggregate = []
 
 with open(root + '../data/video/video_aggregate.tsv') as aggregate_file:
-	aggregate = csv.reader(aggregate_file, delimiter='\t')
+	aggregate_csv = csv.reader(aggregate_file, delimiter='\t')
+
+	for item in aggregate_csv:
+		aggregate.append(item)
+
+
+def render_data_on_clip(base_clip, destination, starts_at=None, during=None, messages_file=None):
+	'''
+	Renders a video with data on it.
+
+	base_clip: The base clip to add data into.
+	starts_at: The rendered clip will start there.
+	during: The rendered clip will be this duration.
+	messages_file: if not none, adds messages from the given srt
+	  file (a pysrt reader object).
+	'''
+	if starts_at is None: starts_at = 0
+	if during is None: during = base_clip.duration
+
+	clip = base_clip.subclip(starts_at, starts_at + during)
+	clip_gui = mpy.ImageClip(root + '../data/video/cadre_données_vide.png').set_duration(clip.duration)
+
+	composition = [clip, clip_gui]
+
+
+	# ------  Data on the video
 
 	print()
-	with click.progressbar(aggregate, length=clip.duration, label='Préparation des données...') as bar:
+	with click.progressbar(aggregate[starts_at:starts_at+during], length=clip.duration, label='Préparation des données...') as bar:
 		for data in bar:
 			second = int(data[0])
-
-			# End reached?
-			if second >= clip.duration:
-				break
 
 			# Altitude
 			missing = data[5] == '-'
@@ -72,7 +81,7 @@ with open(root + '../data/video/video_aggregate.tsv') as aggregate_file:
 					*(ALTITUDE_POSITION if not missing else ALTITUDE_POSITION_MISSING))
 				)
 				.set_duration(1)
-				.set_start(second))
+				.set_start(second - starts_at))
 
 			# Temperature
 			missing = data[4] == '-'
@@ -89,7 +98,7 @@ with open(root + '../data/video/video_aggregate.tsv') as aggregate_file:
 					*(TEMPERATURE_POSITION if not missing else TEMPERATURE_POSITION_MISSING))
 				)
 				.set_duration(1)
-				.set_start(second))
+				.set_start(second - starts_at))
 
 			# Progress bar
 			bar_width = int(math.floor(float(data[2]) * PROGRESS_BAR_WIDTH))
@@ -98,25 +107,54 @@ with open(root + '../data/video/video_aggregate.tsv') as aggregate_file:
 					size=(bar_width, PROGRESS_BAR_HEIGHT),
 					duration=1)
 				.set_pos(PROGRESS_BAR_POSITION)
-				.set_start(second))
+				.set_start(second - starts_at))
 
 			composition += [altitude_text_clip, temperature_text_clip, progress_bar]
 
 
-# ------  Messages above the progress bar (read from a subtitles file)
+	# ------  Messages above the progress bar (read from a subtitles file)
 
-if 'no-messages' not in sys.argv:
-	from render_messages import add_messages_clips
-
-	messages = pysrt.open(root + '../data/video/messages.srt')
-	composition += add_messages_clips(messages, clip.duration)
+	if messages_file is not None:
+		from render_messages import add_messages_clips
+		composition += add_messages_clips(messages_file, clip.duration)
 
 
-# ------  Final render
+	# ------  Final render
 
-print('\nAssemblage de {} clips...\n'.format(len(composition)))
+	print('\nAssemblage de {} clips...\n'.format(len(composition)))
 
-video = mpy.CompositeVideoClip(composition)
-#video.save_frame('frame_na.png', t=0.3)
+	video = mpy.CompositeVideoClip(composition)
+	#video.save_frame('frame_na.png', t=0.3)
 
-video.write_videofile(root + '../../Vidéos/Données brutes/CameraDonnées-complet.mp4', threads=6, preset='medium')
+	video.write_videofile(destination, threads=6, preset='medium', audio=False)
+
+
+def is_int(s):
+    try: 
+        int(s)
+        return True
+    except ValueError:
+        return False
+
+
+if __name__ == '__main__':
+	if 'help' in sys.argv:
+		print(sys.argv[0], '[part duration in seconds [first rendered part (starts at 0)]] [no-messages]')
+		exit()
+
+	clip = mpy.VideoFileClip(root + '../../Enregistrements/Embarqué/CameraBallon-complet.mp4')
+
+	add_messages = 'no-messages' not in sys.argv
+	messages = pysrt.open(root + '../data/video/messages.srt') if add_messages else None
+
+	parts_length = int(sys.argv[1]) if len(sys.argv) > 1 and is_int(sys.argv[1]) else 5*60
+	parts = math.ceil(float(clip.duration) / float(parts_length))
+
+	first_part = int(sys.argv[2]) if len(sys.argv) > 2 and is_int(sys.argv[2]) else 0
+
+	for part in range(first_part, parts):
+		destination = '{}../../Vidéos/Données brutes/Camera-Donnees-{}.mp4'.format(root, part)
+		start = part * parts_length
+
+		print('\n---------------------\n\nRendu de la vidéo {} de {}...'.format(part+1, parts))
+		render_data_on_clip(clip, starts_at=start, during=parts_length, destination=destination)
